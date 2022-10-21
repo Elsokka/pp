f2e6c4596a2c5632b233dc1f6f0322a370d3de0e
Sokka <felipeortegad@gmail.com>
2022-10-20 22:58:10 -0500

    Condicionales

---
+++ js-condicionales.js
@@ -0,0 +1,33 @@
+//Reglas para validar si es verdadero o falso
+
+//If 
+
+if (true) {
+    console.log("Hola");
+}
+
+//else
+
+if (false) {
+    console.log("Hola");
+} else  {
+    console.log("Soy Falso");
+}
+
+// else if 
+
+edad = 18;
+
+if (edad === 20) {
+
+    console.log("Puedes Votar");
+
+} else if(edad > 18){
+    
+    console.log("Puedes votar");
+
+} else  {
+    console.log("No puedes votar");
+    
+}
+
