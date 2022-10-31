84d8578b10fc0d8d5f2e5310ac314bae3c3ce449
Sokka <felipeortegad@gmail.com>
2022-10-31 18:12:22 -0500

    Clase de arrays completa

---
+++ js-arrays.js
@@ -0,0 +1,34 @@
+var frutas = ["manzana", "peras", "bananas", "fresas"]; //Asi se crea un arrays
+
+console.log(frutas[0]); //Para ingresar a un elemento
+
+console.log(frutas.length); //Me dice la cantidad de elementos
+
+//los arrays tiene metodos, son elementos que ayudan agenerar cosas con los arrys
+
+//Agregar elemento
+
+var frutas = ["manzana", "peras", "bananas", "fresas"];
+var masfrutas = frutas.push("Uvas");
+
+//Eliminar un elemento(El ultimo)
+
+var frutas = ["manzana", "peras", "bananas", "fresas"];
+var menosfrutas = frutas.pop();
+
+//Agregar a un valor al arrys al inicio del arrays
+
+var frutas = ["manzana", "peras", "bananas", "fresas"];
+var iniciofruta = frutas.unshift("Uvas");
+
+//Eliminar elemento en el inicio 
+
+var frutas = ["manzana", "peras", "bananas", "fresas"];
+var borrafrutainicio = frutas.unshift("Uvas");
+
+
+//Mutar o saber la posición del elemento del arrays
+
+var frutas = ["manzana", "peras", "bananas", "fresas"];
+var posicion = frutas.indexOf("bananas");
+console.log(posicion)
